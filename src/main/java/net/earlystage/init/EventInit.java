99db966d08a9eca775f3998b29a1297336d263b2
Globox1997 <mitschke11@web.de>
2023-02-16 12:32:22 +0100

    flush

--- src/main/java/net/earlystage/init/EventInit.java
+++ src/main/java/net/earlystage/init/EventInit.java
@@ -4,21 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 import net.earlystage.mixin.access.BlockLootTableGeneratorAccessor;
-import net.earlystage.world.Features;
-import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
 import net.fabricmc.fabric.api.event.registry.RegistryEntryAddedCallback;
 import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
-import net.minecraft.entity.passive.VillagerEntity;
-import net.minecraft.item.FishingRodItem;
 import net.minecraft.item.Items;
 import net.minecraft.loot.LootPool;
 import net.minecraft.loot.entry.ItemEntry;
 import net.minecraft.loot.provider.number.BinomialLootNumberProvider;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.BuiltinRegistries;
 import net.minecraft.util.registry.Registry;
-import net.minecraft.world.WorldEvents;
-import net.minecraft.world.gen.GenerationStep;
 
 public class EventInit {
 
@@ -33,19 +26,6 @@ public class EventInit {
             if (block.getLootTableId().getPath().contains("leaves"))
                 leavesBlockList.add(block.getLootTableId());
         });
-
-        // RegistryEntryAddedCallback.event(Registry.BIOME_KEY).register((rawId, id, biome) -> {
-
-        // if (ConfigInit.CONFIG.generateRocks) {
-        // System.out.println(rawId + " : " + id + " : ");
-
-        // // BiomeModifications.addFeature(WorldInit.rockPredicate, GenerationStep.Feature.TOP_LAYER_MODIFICATION, BuiltinRegistries.PLACED_FEATURE.getKey(Features.ROCK_PLACED_FEATURE).get());
-        // }
-        // // if (ConfigInit.CONFIG.generateFlint)
-        // // BiomeModifications.addFeature(rockPredicate, GenerationStep.Feature.TOP_LAYER_MODIFICATION, BuiltinRegistries.PLACED_FEATURE.getKey(Features.FLINT_PLACED_FEATURE).get());
-        // // if (block.getLootTableId().getPath().contains("leaves"))
-        // // leavesBlockList.add(block.getLootTableId());
-        // });
 
         LootTableEvents.MODIFY.register((resourceManager, lootManager, id, supplier, setter) -> {
             if (leavesBlockList.contains(id)) {
--- src/main/java/net/earlystage/init/RenderInit.java
+++ src/main/java/net/earlystage/init/RenderInit.java
@@ -5,8 +5,8 @@ import net.earlystage.block.render.SieveBlockRenderer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
-import net.fabricmc.fabric.api.client.rendering.v1.BlockEntityRendererRegistry;
 import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.block.entity.BlockEntityRendererFactories;
 
 @Environment(EnvType.CLIENT)
 public class RenderInit {
@@ -15,8 +15,8 @@ public class RenderInit {
         BlockRenderLayerMap.INSTANCE.putBlock(BlockInit.SIEVE, RenderLayer.getCutout());
         BlockRenderLayerMap.INSTANCE.putBlock(BlockInit.REDSTONE_SIEVE, RenderLayer.getCutout());
 
-        BlockEntityRendererRegistry.register(BlockInit.SIEVE_ENTITY, SieveBlockRenderer::new);
-        BlockEntityRendererRegistry.register(BlockInit.CRAFTING_ROCK_ENTITY, CraftingRockBlockRenderer::new);
+        BlockEntityRendererFactories.register(BlockInit.SIEVE_ENTITY, SieveBlockRenderer::new);
+        BlockEntityRendererFactories.register(BlockInit.CRAFTING_ROCK_ENTITY, CraftingRockBlockRenderer::new);
     }
 
 }
