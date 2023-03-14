7de48dd760947f46a705ed531ded6ab9c6fc418c
Globox1997 <mitschke11@web.de>
2023-03-14 20:39:58 +0100

    added wooden shield

--- src/main/java/net/earlystage/init/ItemInit.java
+++ src/main/java/net/earlystage/init/ItemInit.java
@@ -1,9 +1,13 @@
 package net.earlystage.init;
 
+import com.github.crimsondawn45.fabricshieldlib.lib.object.FabricShieldItem;
+
 import net.earlystage.item.*;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.AxeItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Items;
 import net.minecraft.item.PickaxeItem;
 import net.minecraft.item.ShovelItem;
 import net.minecraft.item.SwordItem;
@@ -17,6 +21,9 @@ public class ItemInit {
     public static final Item FLINT_PICKAXE = register("flint_pickaxe", new PickaxeItem(new FlintToolMaterial(), 0, -2.8f, new Item.Settings().group(ItemGroup.TOOLS)));
     public static final Item FLINT_AXE = register("flint_axe", new AxeItem(new FlintToolMaterial(), 1f, -3.2f, new Item.Settings().group(ItemGroup.TOOLS)));
     public static final Item FLINT_HOE = register("flint_hoe", new FlintHoeItem(new FlintToolMaterial(), 0, -3.0f, new Item.Settings().group(ItemGroup.TOOLS)));
+
+    public static final Item WOODEN_SHIELD = register("wooden_shield",
+            new FabricShieldItem(new FabricItemSettings().maxDamage(59).group(ItemGroup.COMBAT), 100, 0, Items.OAK_PLANKS, Items.DARK_OAK_PLANKS, Items.SPRUCE_PLANKS, Items.JUNGLE_PLANKS));
 
     private static Item register(String id, Item item) {
         return register(new Identifier("earlystage", id), item);
